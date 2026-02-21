3f8753bec9b6c5f598a0efe47f8daa1cc246ec5b
VijayKumar.Panga <vijayKumar.panga@entainindia.com>
2026-02-21 14:31:10 +0530

    log mangaer

--- utilities/log_mngr.py
+++ utilities/log_mngr.py
@@ -1,9 +1,34 @@
 import logging
+from datetime import datetime
+import os
 
 _global_logger = None
 
 def setup_custom_logger():
-    return  logging.getLogger()
-
-#  Here we have centralized logging behavior, due to this no need to call logging.getLogger() everywhere
-#  Just return the root logger
+    if _global_logger:
+        return _global_logger
+    
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    
+    if not logger.handlers:
+        log_dir = 'logs'
+        os.makedirs(log_dir, exist_ok=True)
+        log_file = os.path.join(log_dir, f"migration_{datetime.now().strftime('%Y%m%d_%H%M%S')}.log")
+        
+        file_handler = logging.FileHandler(log_file)
+        file_handler.setLevel(logging.DEBUG)
+        
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.INFO)
+        
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        file_handler.setFormatter(formatter)
+        console_handler.setFormatter(formatter)
+        
+        logger.addHandler(file_handler)
+        logger.addHandler(console_handler)
+        
+        logger.info(f"Log file created: {log_file}")
+    
+    return logger
